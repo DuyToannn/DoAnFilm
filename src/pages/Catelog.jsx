1f846551c7973ff9496fa7023dcd750b6a4c2e65
ThanhLoi64 <118653690+ThanhLoi64@users.noreply.github.com>
2022-11-20 19:04:16 +0700

    update catalog,Page Header

--- src/pages/Catelog.jsx
+++ src/pages/Catelog.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 
+import { useParams } from 'react-router';
+import { category as cate } from '../api/tmdbApi';
+import PageHeader from '../components/Page-Header/PageHeader';
+
 const Catelog = () => {
+
+  const { category} = useParams();
+  console.log(category);
+
+  
   return (
-    <div>
-        Catelog
-    </div>
+    <>
+     <PageHeader>
+      {category === cate.movie ? 'Movie' : 'TV Series'}
+     </PageHeader>
+    </>
   )
 }
 
-export default Catelog;
+export default Catelog; 
