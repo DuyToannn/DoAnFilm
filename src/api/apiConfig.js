fdddb0d52485deaa033b10b883481c49b4eb99fb
Duy Toan <76947553+DuyToannn@users.noreply.github.com>
2022-11-22 03:50:14 +0700

    Done swiperslider

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,6 +1,7 @@
 const apiConfig = {
     baseUrl: 'https://api.themoviedb.org/3/',
     apiKey: 'aaa1a416b271fc536562e52b7dcc947b',
+    defaultLanguage: 'vi-VN',
     originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
     w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`
 }
